6e509e6bab3798c0babf02f90035b656a9c261b2
Hermann-Kateu <kateuhermann3@gmail.com>
2022-02-19 17:07:33 +0100

    update exercise2

--- EXO 2.js
+++ EXO 2.js
@@ -1,3 +1,11 @@
-function christMas(Date) {
-    const
-}
+const christMas= (year) =>{
+    const today= new Date();
+    let nextChrist= new Date(`${year}`,11,25);
+    if((nextChrist-today) < 0){
+      return `${-1*Math.ceil((nextChrist-today)/(3600000*24))} days had alredy passed since ${year}`
+    }
+        return `Reamaning ${Math.ceil((nextChrist-today)/(24 * 3600000))} days for the next Christmas`
+   } 
+   console.log(christMas(2022))
+   
+   
